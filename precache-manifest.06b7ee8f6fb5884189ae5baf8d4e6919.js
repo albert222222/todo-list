939af48e39feb6b93ef832761184b3279341362d
Karthikeyan <xmenv22222@gmail.com>
2020-09-30 10:27:30 +0530

    Updates

---
+++ precache-manifest.06b7ee8f6fb5884189ae5baf8d4e6919.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce1fa443546408e53fa381830ef68658",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "562b099c4ae1ff588fe5",
+    "url": "/todo-list/static/css/main.7481c2ea.chunk.css"
+  },
+  {
+    "revision": "1efae2ea0a4dc3e5649e",
+    "url": "/todo-list/static/js/2.4eac4c73.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list/static/js/2.4eac4c73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "562b099c4ae1ff588fe5",
+    "url": "/todo-list/static/js/main.1709925f.chunk.js"
+  },
+  {
+    "revision": "f1c95e41f55d43f946d4",
+    "url": "/todo-list/static/js/runtime-main.d3f0b59c.js"
+  }
+]);
